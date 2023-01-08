69950f7c8a8a9bb81684aad10b0c3274ff4172c3
Lito <lito@eordes.com>
2023-01-08 23:11:07 +0100

    Fixed response print including dash and dollar

--- ChatGPT.py
+++ ChatGPT.py
@@ -4,32 +4,48 @@ import sublime_plugin
 
 import urllib.request as request
 
-settings = sublime.load_settings('ChatGPT.sublime-settings')
-
 class ChatGptCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        self.set_settings()
         self.show_input()
+
+    def set_settings(self):
+        settings = sublime.load_settings('ChatGPT.sublime-settings')
+
+        self.settings = {
+            'api_key': str(settings.get('api_key')),
+            'timeout': int(settings.get('timeout', 10)),
+            'model': str(settings.get('model', 'text-davinci-003')),
+            'temperature': float(settings.get('temperature', 0.5)),
+            'max_tokens': int(settings.get('max_tokens', 1024)),
+            'debug': bool(settings.get('debug', False))
+        }
+
+        self.debug('settings', self.settings)
 
     def show_input(self):
         self.window = sublime.active_window()
         self.view = self.window.active_view()
 
         self.window.show_input_panel(
-            'ChatGPT Request',
+            self.show_input_title(),
             self.show_input_last(),
             self.show_input_done,
             None,
             None
         )
 
+    def show_input_title(self):
+        return 'ChatGPT Question (model: %s | timeout: %s)' % (self.settings['model'], self.settings['timeout'])
+
     def show_input_last(self):
-        if len(settings.get('api_key', '')) == 0:
-            return 'Set the API Key first on package Settings: Menu > Preferences > Package Settingst > ChatGPT > Settings - User'
+        if len(self.settings['api_key']) == 0:
+            return 'You must set the API Key (Preferences > Package Settings > ChatGPT > Settings - User)'
 
         return self.view.settings().get('show_input_last', '')
 
     def show_input_done(self, input_string):
-        if len(settings.get('api_key', '')) == 0:
+        if len(self.settings['api_key']) == 0:
             return
 
         self.debug('show_input_done[input_string]', input_string)
@@ -38,12 +54,17 @@ class ChatGptCommand(sublime_plugin.TextCommand):
             return
 
         self.view.settings().set('show_input_last', input_string)
-        self.view.run_command('insert_snippet', {'contents': self.request(input_string)})
+
+        contents = self.request(input_string).replace('\\', '\\\\').replace('$', '\\$')
+
+        self.debug('show_input_done[contents]', contents)
+
+        self.view.run_command('insert_snippet', {'contents': contents})
 
     def request(self, input_string):
         response = self.request_response()
         data = self.request_data(input_string)
-        timeout = settings.get('timeout', 10)
+        timeout = self.settings['timeout']
 
         self.debug('request[data]', data)
 
@@ -72,18 +93,18 @@ class ChatGptCommand(sublime_plugin.TextCommand):
 
     def request_headers(self):
         return {
-            'Authorization': 'Bearer %s' % settings.get('api_key'),
+            'Authorization': 'Bearer %s' % self.settings['api_key'],
             'Content-Type': 'application/json'
         }
 
     def request_data(self, input_string):
         return json.dumps({
             'prompt': input_string,
-            'model': settings.get('model', 'text-davinci-003'),
-            'temperature': settings.get('temperature', 0.5),
-            'max_tokens': settings.get('max_tokens', 1024)
+            'model': self.settings['model'],
+            'temperature': self.settings['temperature'],
+            'max_tokens': self.settings['max_tokens']
         }).encode()
 
     def debug(self, key, value):
-        if (settings.get('debug', False)):
+        if (self.settings['debug']):
             print(key, value)
