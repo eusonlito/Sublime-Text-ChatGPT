b6d4e6878788d607d298a91fc11d8bac66c3d36a
Lito <lito@eordes.com>
2023-01-08 01:26:38 +0100

    Improved Plugin Code

--- ChatGPT.py
+++ ChatGPT.py
@@ -6,44 +6,81 @@ import urllib.request as request
 
 settings = sublime.load_settings('ChatGPT.sublime-settings')
 
-def chatgpt_request():
-    sublime.active_window().show_input_panel('ChatGPT Request', '', on_done, on_change, on_cancel)
-
-def on_done(input_string):
-    data = json.dumps({
-        'prompt': input_string,
-        'model': settings.get('model'),
-        'temperature': settings.get('temperature', 0.5),
-        'max_tokens': settings.get('max_tokens', 1024)
-    }).encode()
-
-    headers = {
-        'Authorization': 'Bearer %s' % settings.get('api_key'),
-        'Content-Type': 'application/json'
-    }
-
-    response = request.Request(
-        url='https://api.openai.com/v1/completions',
-        method='POST',
-        headers=headers
-    )
-
-    timeout = settings.get('timeout', 10)
-
-    try:
-        text = request.urlopen(response, data=data, timeout=timeout).read().decode('utf-8')
-        text = json.loads(text)['choices'][0]['text']
-    except Exception as e:
-        text = '# Error: %s #' % str(e)
-
-    sublime.active_window().active_view().run_command('insert_snippet', {'contents': text})
-
-def on_change(input_string):
-    pass
-
-def on_cancel():
-    pass
-
 class ChatGptCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        chatgpt_request()
+        self.show_input()
+
+    def show_input(self):
+        self.window = sublime.active_window()
+        self.view = self.window.active_view()
+
+        self.window.show_input_panel(
+            'ChatGPT Request',
+            self.show_input_last(),
+            self.show_input_done,
+            None,
+            None
+        )
+
+    def show_input_last(self):
+        if len(settings.get('api_key', '')) == 0:
+            return 'Set the API Key first on package Settings: Menu > Preferences > Package Settingst > ChatGPT > Settings - User'
+
+        return self.view.settings().get('show_input_last', '')
+
+    def show_input_done(self, input_string):
+        if len(settings.get('api_key', '')) == 0:
+            return
+
+        self.debug('show_input_done[input_string]', input_string)
+
+        if len(input_string) == 0:
+            return
+
+        self.view.settings().set('show_input_last', input_string)
+        self.view.run_command('insert_snippet', {'contents': self.request(input_string)})
+
+    def request(self, input_string):
+        response = self.request_response()
+        data = self.request_data(input_string)
+        timeout = settings.get('timeout', 10)
+
+        self.debug('request[data]', data)
+
+        try:
+            text = request.urlopen(response, data=data, timeout=timeout).read().decode('utf-8')
+            text = str(json.loads(text)['choices'][0]['text'])
+
+            if len(text) == 0:
+                text = '# No Response #'
+        except Exception as e:
+            text = '# Error: %s #' % str(e)
+
+        self.debug('request[text]', text)
+
+        return text
+
+    def request_response(self):
+        return request.Request(
+            url='https://api.openai.com/v1/completions',
+            method='POST',
+            headers=self.request_headers()
+        )
+
+    def request_headers(self):
+        return {
+            'Authorization': 'Bearer %s' % settings.get('api_key'),
+            'Content-Type': 'application/json'
+        }
+
+    def request_data(self, input_string):
+        return json.dumps({
+            'prompt': input_string,
+            'model': settings.get('model', 'text-davinci-003'),
+            'temperature': settings.get('temperature', 0.5),
+            'max_tokens': settings.get('max_tokens', 1024)
+        }).encode()
+
+    def debug(self, key, value):
+        if (settings.get('debug', False)):
+            print(key, value)
